c16cd14517523e73d655a0049fe302fd49820e40
connellyj <juliakconnelly@gmail.com>
2017-05-22 17:13:16 -0500

    cleaned up linkedlist

--- linkedlist.c
+++ linkedlist.c
@@ -6,6 +6,7 @@
 #include "value.h"
 #include "talloc.h"
 #include "interpreter.h"
+#include "linkedlist.h"
 
 // Helper function to get the car of a "cons cell"
 Value *car(Value *list) {
@@ -19,6 +20,20 @@ Value *cdr(Value *list) {
     assert(list);
     assert(list->type == CONS_TYPE);
     return list->c.cdr;
+}
+
+// Helper function to get the variable of a binding
+Value *var(Value *binding) {
+    assert(binding);
+    assert(binding->type == BINDING_TYPE);
+    return binding->b.var;
+}
+
+// Helper function to get the value of a binding
+Value *val(Value *binding) {
+    assert(binding);
+    assert(binding->type == BINDING_TYPE);
+    return binding->b.val;
 }
 
 // Helper function to set the car of a "cons cell"
@@ -62,31 +77,6 @@ Value *makeNull() {
     return nullValue;
 }
 
-// Create a new CONS_TYPE value node
-Value *cons(Value *car, Value *cdr) {
-    assert(car);
-    assert(cdr);
-    Value *consValue = (Value *)talloc(sizeof(Value));
-    consValue->type = CONS_TYPE;
-    setCar(consValue, car);
-    setCdr(consValue, cdr);
-    return consValue;
-}
-
-// Helper function to get the variables of a binding
-Value *var(Value *binding) {
-    assert(binding);
-    assert(binding->type == BINDING_TYPE);
-    return binding->b.var;
-}
-
-// Helper function to get the value of a binding
-Value *val(Value *binding) {
-    assert(binding);
-    assert(binding->type == BINDING_TYPE);
-    return binding->b.val;
-}
-
 // Creates a BINDING_TYPE Value node
 Value *makeBinding(Value *var, Value *val) {
     assert(var);
@@ -105,6 +95,7 @@ Value *makeVoid() {
     return voidValue;
 }
 
+// Creates a closure type Value node
 Value *makeClosure(Value *paramNames, Value *functionCode, Frame *frame) {
     assert(paramNames);
     assert(functionCode);
@@ -119,63 +110,92 @@ Value *makeClosure(Value *paramNames, Value *functionCode, Frame *frame) {
     return closure;
 }
 
+// Create a new CONS_TYPE value node
+Value *cons(Value *car, Value *cdr) {
+    assert(car);
+    assert(cdr);
+    Value *consValue = (Value *)talloc(sizeof(Value));
+    consValue->type = CONS_TYPE;
+    setCar(consValue, car);
+    setCdr(consValue, cdr);
+    return consValue;
+}
+
+// Helper function to print a boolean
+void displayBool(Value *boolVal) {
+    assert(boolVal);
+    assert(boolVal->type == BOOL_TYPE);
+    if(boolVal->i) printf("#t");
+    else printf("#f");
+}
+
+// Helper function to display a binding
+void displayBinding(Value *binding) {
+    assert(binding);
+    assert(binding->type == BINDING_TYPE);
+    printf("[");
+    displayList(var(binding), false);
+    printf(" = ");
+    displayList(val(binding), false);
+    printf("]");
+}
+
+// Helper function to display nested lists
+void displayNestedList(Value *list) {
+    assert(list);
+    assert(list->type == CONS_TYPE);
+    bool print = (car(list))->type == CONS_TYPE;
+    bool space = !isNull(cdr(list));
+    if(print) printf("(");
+    displayList(car(list), space);
+    if(print) {
+        if(space) printf(") ");
+        else printf(")");
+    }
+    if(space) displayList(cdr(list), false);
+}
+
 // Helper function to display a list of value nodes
 void displayList(Value *list, bool addSpace) {
-    if(list->type != VOID_TYPE) {
-        if(list->type != CONS_TYPE) {
-            if(list->type == INT_TYPE) printf("%i", list->i);
-            else if (list->type == DOUBLE_TYPE) printf("%f", list->d);
-            else if (list->type == STR_TYPE || list->type == OPEN_TYPE ||
-                list->type == CLOSE_TYPE || list->type == SYMBOL_TYPE) {
-                printf("%s", list->s);
-            }
-            else if(list->type == BOOL_TYPE) {
-                if(list->i) printf("#t");
-                else printf("#f");
-            }
-            else if(list->type == BINDING_TYPE) {
-                displayList(var(list), false);
-                printf("=");
-                displayList(val(list), true);
-            }
-            else if(list->type == NULL_TYPE) printf("()");
-            if(addSpace) printf(" ");
+    assert(list);
+    if(list->type == VOID_TYPE) return;
+    if(list->type != CONS_TYPE) {
+        if(list->type == INT_TYPE) printf("%i", list->i);
+        else if (list->type == DOUBLE_TYPE) printf("%f", list->d);
+        else if(list->type == NULL_TYPE) printf("()");
+        else if(list->type == PTR_TYPE) printf("%p", list->p);
+        else if(list->type == CLOSURE_TYPE) printf("closure");
+        else if(list->type == BOOL_TYPE) displayBool(list);
+        else if(list->type == BINDING_TYPE) displayBinding(list);
+        else if (list->type == STR_TYPE || list->type == OPEN_TYPE ||
+            list->type == CLOSE_TYPE || list->type == SYMBOL_TYPE) {
+            printf("%s", list->s);
         }
-        else if (list->type == CONS_TYPE) {
-            bool print = (car(list))->type == CONS_TYPE;
-            bool space = !isNull(cdr(list));
-            if(print) printf("(");
-            displayList(car(list), space);
-            if(print) {
-                if(space) printf(") ");
-                else printf(")");
-            }
-            if(cdr(list)->type != NULL_TYPE) displayList(cdr(list), true);
-        }
+        if(addSpace) printf(" ");
     }
+    else displayNestedList(list);
 }
 
 // Displays the given list on one line with parentheses denoting lists
 void display(Value *list) {
-    if(!list) return;
+    assert(list);
     if(list->type == CONS_TYPE) {
         bool space = !isNull(cdr(list));
         printf("(");
         displayList(car(list), space);
-        displayList(cdr(list), space);
+        displayList(cdr(list), false);
         printf(") ");
-    }else displayList(list, true);
+    } else displayList(list, true);
 }
 
 // Helper method to copy a CONS_TYPE Value node
 Value *copyConsValue(Value *val) {
     assert(val);
-    Value *copy = talloc(sizeof(Value));
+    assert(val->type == CONS_TYPE);
+    Value *copy = (Value *)talloc(sizeof(Value));
     copy->type = val->type;
-    if(copy->type == CONS_TYPE) {
-        setCar(copy, car(val));
-        setCdr(copy, cdr(val));
-    }
+    setCar(copy, car(val));
+    setCdr(copy, cdr(val));
     return copy;
 }
 
@@ -183,6 +203,8 @@ Value *copyConsValue(Value *val) {
 Value *reverse(Value *list) {
     assert(list);
     if(isNull(list)) return list;
+    assert(list->type == CONS_TYPE);
+
     Value *cur = copyConsValue(list);
     Value *next = copyConsValue(cdr(cur));
     Value *prev;
@@ -190,6 +212,7 @@ Value *reverse(Value *list) {
     prev = cur;
     cur = next;
     while(!isNull(cur)) {
+        assert(cur->type == CONS_TYPE);
         next = copyConsValue(cdr(cur));
         setCdr(cur, prev);
         prev = cur;
--- linkedlist.h
+++ linkedlist.h
@@ -13,6 +13,9 @@ Value *cons(Value *newCar, Value *newCdr);
 // Display the contents of the linked list to the screen in some kind of readable format
 void display(Value *list);
 
+// Helper function to display a list of value nodes
+void displayList(Value *list, bool addSpace);
+
 // Return a new list that is the reverse of the one that is passed in. No stored
 // data within the linked list should be duplicated; rather, a new linked list
 // of CONS_TYPE nodes should be created, that point to items in the original
@@ -27,18 +30,25 @@ Value *car(Value *list);
 // that this is a legitimate operation.
 Value *cdr(Value *list);
 
+// Helper function to set the cdr of a "cons cell"
 void setCdr(Value *list, Value *newCdr);
 
+// Helper function to set the car of a "cons cell"
 void setCar(Value *list, Value *newCar);
 
+// Helper function to get the variable of a binding
 Value *var(Value *binding);
 
+// Helper function to get the value of a binding
 Value *val(Value *binding);
 
+// Creates a BINDING_TYPE Value node
 Value *makeBinding(Value *var, Value *val);
 
+// Creates a VOID_TYPE Value node
 Value *makeVoid();
 
+// Creates a closure type Value node
 Value *makeClosure(Value *paramNames, Value *functionCode, struct Frame *frame);
 
 // Utility to check if pointing to a NULL_TYPE value. Use assertions to make sure
